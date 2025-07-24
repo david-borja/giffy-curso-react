e7c41f0fedd8a352a204f5bbedb0e8e71a5164be
david-borja <davidborjamartinez@gmail.com>
2025-07-24 19:41:46 +0200

    refactor(): category component

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'wouter'
+
+export default function Category({ name, options = [] }) {
+  return (
+    <>
+      <h3 className='App-title'>{name}</h3>
+      <ul>
+        {options.map((singleOption) => (
+          <li key={singleOption}>
+            <Link to={`/search/${singleOption}`}>{singleOption}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
