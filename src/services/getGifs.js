d279e50252b6844263c144b0562575d26ab0c084
david-borja <davidborjamartinez@gmail.com>
2025-07-25 13:32:59 +0200

    refactor(): getGifs

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,18 +1,22 @@
 const apiKey = import.meta.env.VITE_GIPHY_API_KEY
 
-export default function getGifs({ keyword = 'morty' } = {}) {
-  const SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
-
-  return fetch(SEARCH_URL)
-    .then((res) => res.json())
-    .then(({ data }) => {
-      if (Array.isArray(data)) {
-        const gifs = data.map((image) => {
-          const { images, title, id } = image
-          const { url } = images.downsized_medium
-          return { url, title, id }
-        })
-        return gifs
-      }
+const fromApiResponseToGifs = (apiResponse) => {
+  const { data = [] } = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map((image) => {
+      const { images, title, id } = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
     })
+    return gifs
+  }
+  return []
+}
+
+export default function getGifs({ limit = 25, keyword = 'morty' } = {}) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=G&lang=en`
+
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then(fromApiResponseToGifs)
 }
